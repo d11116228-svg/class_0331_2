87096f177e2544e7d27920a9d4f00a73cabef2e4
d11116228-svg <d11116228@gs.takming.edu.tw>
2026-03-31 16:17:07 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+// 父類別
+public class Person
+{
+    public string Name = "姓氏";
+    //建構子
+    public Person(string n)
+    {
+        Name = n;
+    }
+}
+
+// 子類別
+public class Student : Person
+{
+    public string stdname = "名字";
+    //建構子
+    public Student(string n) : base("陳")
+    {
+        stdname = Name + n;
+    }
+}
